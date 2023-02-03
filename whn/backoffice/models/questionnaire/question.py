085ea3eba7d3bc3b81f252206bf8e22c2fc39e1c
Nardo Bailey <baileynardo@gmail.com>
2023-02-03 09:50:22 -0500

    Updated models
    Updated init files. Added more detail to Question class.

--- whn/backoffice/models/questionnaire/question.py
+++ whn/backoffice/models/questionnaire/question.py
@@ -1,24 +1,54 @@
 from django.db import models
+from .answers.answer import Answer
 
 
 class Question(models.Model):
+    """Represents a question in a questionnaire. The answers of which will form a part
+    of the patient record and instruct practitioners on diagnosis and treatment options
+    which will form a part of """
+    # List of question types
     COMMUNITY_ENVIRONMENT = 'CE'
     HEALTH_SEEKING_BEHAVIOUR = 'HE'
     HOUSEHOLD = 'HH'
     SOCIAL_INCLUSION = 'SI'
 
-    id = models.UUIDField()
+    # List of answer types
+    CHOICE = 'CH'
+    FLOAT = 'FL'
+    FREE_TYPE = 'FT'
+    MULTI_CHOICE = 'MC'
+    MULTIPLE_CHOICE_TEXT = 'MT'
+    NUMBER = 'NB'
+    TEXT = 'TX'
+
     QUESTION_TYPE_CHOICES = [
         (COMMUNITY_ENVIRONMENT, 'Community Environment'),
         (HEALTH_SEEKING_BEHAVIOUR, 'Health Seeking Behaviour'),
         (HOUSEHOLD, 'Household Data'),
         (SOCIAL_INCLUSION, 'Social Inclusion'),
     ]
+
+    ANSWER_TYPE_CHOICES = [
+        (CHOICE, 'Choice Field'),
+        (FLOAT, 'Floating Point Number Field'),
+        (FREE_TYPE, 'Free Type Field'),
+        (MULTI_CHOICE, 'Multiple Choice Field'),
+        (MULTIPLE_CHOICE_TEXT, 'Multiple Choice Text Field'),
+        (NUMBER, 'Number Field'),
+        (TEXT, 'Text Field'),
+    ]
+
+    id = models.UUIDField()
     category = models.CharField(
         max_length=2,
         choices=QUESTION_TYPE_CHOICES,
     )
+    answer_type = models.CharField(
+        max_length=2,
+        choices=ANSWER_TYPE_CHOICES,
+    )
     question_body = models.CharField(max_length=255)
-    # choices = models.Many
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+# ToDo: Add Method to retrieve answer object based on choice.
