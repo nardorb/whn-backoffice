908d7b592e27d2610fe3293398e2be22eed459ed
Nardo <nardorb@users.noreply.github.com>
2023-04-11 09:22:17 -0500

    Update settings.py
    Set key from nonsense string to None

--- whn/whn-env/settings.py
+++ whn/whn-env/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lol'
+SECRET_KEY = None   # Add credentials variable when env injection is complete.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
